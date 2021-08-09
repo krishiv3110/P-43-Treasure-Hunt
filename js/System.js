1e1c1b6cdb013befe9fd53e826836ee3238d6ac5
krishiv3110 <75845762+krishiv3110@users.noreply.github.com>
2021-08-09 17:17:40 +0530

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,22 @@
+class System{
+
+    constructor(){}
+  
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        fill("green");
+        text(code,300,300);
+        
+//to return true if correct answer entered...
+
+       if(actualCode === enteredCode.toUpperCase()){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+        
+    }
+
+}
